20f8fd2bafe05b050e8cc0f3682e687d50bf6304
Caio Lagreca <caiomiranda701@hotmail.com>
2024-08-26 21:27:45 +1000

    first commit

---
+++ Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace StripeApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string PriceId { get; set; }
+
+    }
+}
